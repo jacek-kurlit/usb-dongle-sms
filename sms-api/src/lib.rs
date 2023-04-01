0983a73363b032c0a78ba5c264e6ba383f6d9850
Jacek Kurlit <jacek.kurlit@kitopi.com>
2023-04-01 15:09:03 +0200

    test1

--- sms-api/src/lib.rs
+++ sms-api/src/lib.rs
@@ -1,4 +1,5 @@
 pub fn send_sms(message: String, receivers: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     println!("Sending {message} to {} receivers", receivers.len());
+    // testing stuff here
     Ok(())
 }
