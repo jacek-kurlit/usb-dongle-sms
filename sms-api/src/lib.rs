44e4628ccc6208ed2805e0f026fa899c999d581b
Jacek Kurlit <jacek.kurlit@kitopi.com>
2023-04-01 18:23:31 +0200

    test

--- sms-api/src/lib.rs
+++ sms-api/src/lib.rs
@@ -2,3 +2,14 @@ pub fn send_sms(message: String, receivers: Vec<String>) -> Result<(), Box<dyn s
     println!("Sending {message} to {} receivers", receivers.len());
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_send_sms() {
+        send_sms("Hello World".to_string(), vec![]).unwrap();
+    }
+}
